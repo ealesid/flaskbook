d59a91edddec98c0cdb3c399a26ce02973050844
ealesid <ealesid@gmail.com>
2016-05-24 01:52:17 +0300

    p1ch3: templates before bootstrap

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -8,14 +8,15 @@ manager = Manager(app)
 @app.route('/')
 def index():
     ua = request.headers.get('User-Agent')
-    return '<h1>Hello!!!</h1><p>Your browser is %s</p>' % ua
+    # return '<h1>Hello!!!</h1><p>Your browser is %s</p>' % ua
+    return render_template('index.html', ua=ua)
 
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
-
+    # return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     # app.run(debug=True)
