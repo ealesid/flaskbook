aaa639943c6b6d3d5703b1d075b732c183a7a5f7
ealesid <ealesid@gmail.com>
2016-05-24 04:21:08 +0300

    p1ch4: message flashing

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, redirect, url_for
+from flask import Flask, request, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
@@ -22,6 +22,9 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
