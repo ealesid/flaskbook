56b3014ca8858ea8d2d2d9978e0465dbc537c8dc
ealesid <ealesid@gmail.com>
2016-05-24 03:44:46 +0300

    p1ch4: form handling

--- run.py
+++ run.py
@@ -1,17 +1,32 @@
 from flask import Flask, request, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
 manager = Manager(app)
 bs = Bootstrap(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
     ua = request.headers.get('User-Agent')
     # return '<h1>Hello!!!</h1><p>Your browser is %s</p>' % ua
-    return render_template('index.html', ua=ua)
+    return render_template('index.html', ua=ua, form=form, name=name)
 
 
 
@@ -33,6 +48,6 @@ def internal_server_error(e):
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    manager.run()           # to run in command line as 'python3 ./run.py runserver'
+    app.run(debug=True)
+    # manager.run()           # to run in command line as 'python3 ./run.py runserver'
 
