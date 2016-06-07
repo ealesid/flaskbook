cdde6a129680f89df9e2acaf3aa66725a05566e9
ealesid <ealesid@gmail.com>
2016-06-07 21:34:04 +0300

    8f-p2ch8: password updates

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,9 +13,10 @@ class LoginForm(Form):
 
 
 class RegistrationForm(Form):
-    email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField('Username', validators=[
-        DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Username must have only letters, numbers, dots or underscores')])
+        DataRequired(), Length(1, 64),
+        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Username must have only letters, numbers, dots or underscores')])
     password = PasswordField('Password', validators=[
         DataRequired(), EqualTo('password2', message='Passwords must match!!!')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
@@ -26,6 +27,17 @@ def validate_email(self, field):
     if User.objects(email__exact=field.data).first():
         raise ValidationError('Email already registered')
 
+
 def validate_username(self, field):
     if User.objects(username__exact=field.data).first():
         raise ValidationError('Username already in use')
+
+
+class ChangePasswordForm(Form):
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField(
+        'New password',
+        validators=[DataRequired(), EqualTo('password2', message='Passwords must match')]
+    )
+    password2 = PasswordField('Confirm new password', validators=[DataRequired()])
+    submit = SubmitField('Update Password')
